87c83a597142a068c1e2a642067e713aef8a1aa9
mathenn <matheusjhavila@gmail.com>
2026-03-23 09:23:55 -0300

    Dependency injection, API controller and route added

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CadAPI.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-[ApiController]
-[Route("api/clientes")]
-public class ClientesController : ControllerBase
+[ApiController] // indica que é o controller da API, habilitando comportamentos automáticos
+[Route("api/clientes")] // define rota base para todos os endpoints deste controller
+public class ClientesController : ControllerBase // classe base otimizada para APIs
 {
     private readonly AppDbContext _context;
 
@@ -10,7 +11,19 @@ public class ClientesController : ControllerBase
     {
         _context = context;
     }
-    
+
+    [HttpPost]
+    public IActionResult Criar(CriarClienteDto dto)
+    {
+        var cliente = Cliente.Criar(
+            dto.Nome, dto.Cpf,
+            dto.Email, dto.DataNascimento);
+        
+        _context.Cliente.Add(cliente);
+        _context.SaveChanges();
+        return Ok(cliente);
+
+    }
     
 }
 
