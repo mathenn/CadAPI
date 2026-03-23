5efdbe6427878de5cfe90ff71cb61d3cd0681e62
mathenn <matheusjhavila@gmail.com>
2026-03-23 11:56:18 -0300

    dto validation created

--- Application/DTOs/CriarClienteDto.cs
+++ Application/DTOs/CriarClienteDto.cs
@@ -1,10 +1,19 @@
-﻿namespace CadAPI.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadAPI.Application.DTOs
 {
     public class CriarClienteDto
     {
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(100, MinimumLength = 3)]
         public string Nome { get; set; }
+        [Required]
+        [CpfValidation] // validação simples
         public string Cpf { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public DateTime DataNascimento { get; set; }
         
     }
